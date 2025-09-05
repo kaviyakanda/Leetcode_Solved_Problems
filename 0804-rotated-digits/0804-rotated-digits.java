0140b3597bb98ebb68e18a55c846073c1cd5ecc0
kaviyakanda <158142238+kaviyakanda@users.noreply.github.com>
2025-09-05 23:30:01 +0530

    Time: 11 ms (17.27%), Space: 44 MB (21.55%) - LeetHub

---
+++ 0804-rotated-digits/0804-rotated-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            String num = String.valueOf(i);
+            
+            if (num.contains("3") || num.contains("4") || num.contains("7")) {
+                continue; 
+            }
+            
+            if (num.contains("2") || num.contains("5") || num.contains("6") || num.contains("9")) {
+                count++; 
+            }
+        }
+        return count;
+    }
+}
